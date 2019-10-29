29e25125b64a9deb7f92b7b79170c56a9d25f1b7
tcampeol <toni.campeol@hauteloire.fr>
2019-10-29 15:00:34 +0100

    Removed deprecated test libraries and added pseudo unit test.

--- app/src/test/java/com/example/mytestapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mytestapp/ExampleUnitTest.kt
@@ -1,16 +1,18 @@
 package com.example.mytestapp
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class ExampleUnitTest
+{
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
+    fun addition_isCorrect() = assertEquals(4, 2 + 2)
+
+    @Test
+    fun isTestWorking() = assertEquals("test", "test")
 }
